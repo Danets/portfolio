b4e27203b9041a9766b714a03f163395cedc83c5
Valeriy Danets <dantesvv@gmail.com>
2023-05-10 15:02:58 +0300

    Was established starter template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import  { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState('');
+
+  const handlerClick = () => {
+    setCount(count + 1);
+    setColor('App-btn')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>You cliced {count} times</p>
+      <button className={color} onClick={handlerClick}>
+        Click me!
+      </button>  
     </div>
   );
 }
