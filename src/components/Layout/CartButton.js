059d554d9f9c6e7ba2d7852c021a8fe42eac70b8
Valeriy Danets <dantesvv@gmail.com>
2023-06-05 09:35:58 +0300

    Was added animation for CartButton component

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,20 +1,34 @@
 import styles from "./CartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import { CartContext } from "../../context/CartContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const CartButton = (props) => {
   const cartContext = useContext(CartContext);
+  const [isCartChanged, setCartChanged] = useState(false);
+
+  const animatedBadge = `${styles.button} ${isCartChanged ? styles.bump : ''}`
+
   const quantityMeals = cartContext.meals.reduce((acc, next) => {
     return acc + next.amount;
-  }, 0)
-  
+  }, 0);
+
+  useEffect(() => {
+    setCartChanged(true);
+
+    const timerId = setTimeout(() => {
+      setCartChanged(false);
+    }, 300);
+
+    return () => clearTimeout(timerId);
+  }, [quantityMeals]);
+
   return (
-    <button onClick={props.onOpenModal} className={styles.button}>
+    <button onClick={props.onOpenModal} className={animatedBadge}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
-      {/* <span>Cart</span> */}
+      <span>Cart</span>
       <span className={styles.badge}>{quantityMeals}</span>
     </button>
   );
