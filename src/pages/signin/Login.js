5c835e784858a99b23cecde8be3a3f87c29bdc5d
Valeriy Danets <dantesvv@gmail.com>
2023-06-12 14:30:38 +0300

    Was added validation for Login component

--- src/pages/signin/Login.js
+++ src/pages/signin/Login.js
@@ -1,44 +1,82 @@
 import { useState } from "react";
-import "./Login.css";
+import styles from "./Login.module.css";
 
 const Login = () => {
   const [data, setFormdata] = useState({ username: "", password: "" });
+  const [isTouchedUsername, setTouchUsername] = useState(false);
+  const [isTouchedPassword, setTouchPassword] = useState(false);
+  const isUsernameValid = data.username.trim() !== "";
+  const isPasswordValid = data.password.trim() !== "";
+  const isUsernameInvalid = !isUsernameValid && isTouchedUsername;
+  const isPassswordInValid = !isPasswordValid && isTouchedPassword;
+
+  const classErrorName = isUsernameInvalid ? `${styles['form-control-error']}` : '';
+  const classErrorPass = isPassswordInValid ? `${styles['form-control-error']}` : '';
+
+  let isFormValid = false;
+
+  if (isUsernameValid && isPasswordValid) {
+    isFormValid = true;
+  }
+
+  const handleUsernameFocus = () => {
+    setTouchUsername(true);
+  };
+
+  const handlePasswwordFocus = () => {
+    setTouchPassword(true);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(data);
+    setTouchUsername(true);
+    setTouchPassword(true);
+    if (!isUsernameValid && !isPasswordValid) return;
     setFormdata({
       username: "",
       password: "",
     });
+    setTouchUsername(false);
+    setTouchPassword(false);
   };
+
   const handleChangeInput = (event, name) => {
     setFormdata((prevstate) => ({
       ...prevstate,
       [name]: event.target.value,
     }));
   };
+
   return (
     <>
       <h3>Login Form</h3>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter Username"
-          required
-          value={data.username}
-          onChange={(e) => handleChangeInput(e, "username")}
-        />
+        <div className={classErrorName}>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            required
+            value={data.username}
+            onChange={(e) => handleChangeInput(e, "username")}
+            onBlur={handleUsernameFocus}
+          />
+          {isUsernameInvalid && <div>Put name</div>}
+        </div>
 
-        <input
-          type="password"
-          placeholder="Enter Password"
-          required
-          value={data.password}
-          onChange={(e) => handleChangeInput(e, "password")}
-        />
-
-        <button type="submit">Login</button>
+        <div className={classErrorPass}>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            required
+            value={data.password}
+            onChange={(e) => handleChangeInput(e, "password")}
+            onBlur={handlePasswwordFocus}
+          />
+          {isPassswordInValid && <div>Put Password</div>}
+        </div>
+        <button type="submit" disabled={!isFormValid}>
+          Login
+        </button>
       </form>
     </>
   );
