5687a4f5618ef3abc46bb674fa539fa98e41ca6b
Valeriy Danets <dantesvv@gmail.com>
2023-07-07 18:00:59 +0300

    was fixed of getting data after reloading the PostPage

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useMemo, useCallback } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+
 import queryString from "query-string";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -20,11 +21,13 @@ const PostPage = () => {
     keysPosts = Object.keys(posts[0]);
   }
 
+  // LOCATION
   const location = useLocation();
   const query = queryString.parse(location.search);
 
   const navigate = useNavigate();
 
+  // SORTING LOGIC
   const onSortPosts = (arr, key) => {
     const sortedArr = [...arr];
     if (!key || !keysPosts.includes(key)) {
@@ -55,9 +58,11 @@ const PostPage = () => {
   useEffect(() => {
     dispatch(getPostsAsync())
       .unwrap()
-      .then(() => toast("Posts Loaded!"))
+      .then((res) => {
+        setSortedArr(res);
+        toast("Posts Loaded!")}
+        )
       .catch((err) => toast(err));
-    setSortedArr(entities);
   }, [dispatch]);
 
   useEffect(() => {
@@ -73,7 +78,7 @@ const PostPage = () => {
       <ToastContainer />
       {error && <h2>{error}</h2>}
       {isLoading && <h2>Loading...</h2>}
-      {!isLoading && !error && (
+      {!isLoading && (
         <>
           <h3>
             {queryKey ? `Posts were sorted by ${queryKey}` : "No queryKey"}
