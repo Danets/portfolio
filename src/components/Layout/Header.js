dda84614d682e891fcefbaf78f387bee086d67bf
Valeriy Danets <dantesvv@gmail.com>
2023-07-18 17:18:23 +0300

    Was developed Drawer aka SideBar

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,20 +1,150 @@
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import HouseIcon from "@mui/icons-material/House";
+import FoodBankIcon from "@mui/icons-material/FoodBank";
+import TaskIcon from "@mui/icons-material/Task";
+import PostAddIcon from "@mui/icons-material/PostAdd";
+import LoginIcon from "@mui/icons-material/Login";
+import HowToRegIcon from "@mui/icons-material/HowToReg";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
 import styles from "./Header.module.css";
 import Navigation from "./Navigation";
 import CartButton from "./CartButton";
 import sushiImg from "../../assets/sushi.jpg";
 
-const Header = ({onOpenModal}) => {
-  return <>
-   <header className={styles.header}>
-    <h1>Logo</h1>
-     <Navigation />
-     <CartButton onOpenModal={onOpenModal}/>
-  </header>
-  <div className={styles['main-image']}>
-    <img src={sushiImg} alt="Sushi" />
-  </div>
-  </>
- 
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+const Header = ({ onOpenModal }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          <AppBar position="fixed" open={open}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                className={styles.btn}
+                edge="start"
+                sx={{ mr: 2, ...(open && { display: "none" }) }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" noWrap component="div">
+                <div className={styles.wrapper}>
+                  <Navigation />
+                  <CartButton onOpenModal={onOpenModal} />
+                </div>
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            sx={{
+              width: drawerWidth,
+              flexShrink: 0,
+              "& .MuiDrawer-paper": {
+                width: drawerWidth,
+                boxSizing: "border-box",
+              },
+            }}
+            variant="persistent"
+            anchor="left"
+            open={open}
+          >
+            <DrawerHeader>
+              <IconButton onClick={handleDrawerClose}>
+                <CloseIcon />
+              </IconButton>
+            </DrawerHeader>
+            <Divider />
+            <List>
+              {["Home", "Food", "Tasks", "Posts"].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index === 0 ? <HouseIcon /> : ""}
+                      {index === 1 ? <FoodBankIcon /> : ""}
+                      {index === 2 ? <TaskIcon /> : ""}
+                      {index === 3 ? <PostAddIcon /> : ""}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+            <Divider />
+            <List>
+              {["Login", "Signup"].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index === 0 ? <LoginIcon /> : ""}
+                      {index === 1 ? <HowToRegIcon /> : ""}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
+        </Box>
+      <div className={styles["main-image"]}>
+        <img src={sushiImg} alt="Sushi" />
+      </div>
+    </>
+  );
 };
 
 export default Header;
